4c799444d3ec1016df415626ddd5023cc10a5328
harman-brar <harmandeep.brar@alumni.ubc.ca>
2020-11-20 17:14:13 -0800

    updated queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -7,7 +7,7 @@ INSERT INTO Books (UserId, HotelId, SuiteNo, BookingDate, StayLength, Price) VAL
 UPDATE Suite SET IsInUse = 0 WHERE SuiteNo = 2 AND HotelId = 4;
 
 /* DELETE */
-DELETE FROM Pool WHERE HotelId = 3;
+DELETE FROM Pool WHERE HotelId = 3 AND PoolName = X;
 
 /* PROJECTION */
 SELECT Name, Email, Phone FROM Guest;
@@ -18,14 +18,10 @@ SELECT Phone FROM Guest WHERE Email = 'larry@dairy.com';
 /* JOIN */
 SELECT W.HotelId, W.GymName, G.MaxCapacity 
 FROM WorksOutAt W, Gym G
-WHERE W.GymName = G.GymName;
+WHERE W.GymName = G.GymName AND W.UserId = X;
 
 /* DIVISION */
-SELECT UserId, Name 
-FROM Guest G
-WHERE NOT EXISTS(	
-		SELECT P.PID 
-		FROM Performance EXCEPT(	
-					SELECT W.PID
-					FROM Watches
-					WHERE W.UserId = G.UserId))) 
+SELECT UserId, Name FROM Guest G
+WHERE NOT EXISTS((SELECT P.PID FROM Performance) 
+EXCEPT
+(SELECT W.PID FROM Watches WHERE W.UserId = G.UserId)); 
